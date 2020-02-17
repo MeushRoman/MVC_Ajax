3fa6e6b6f26ad6996d9f7a29a2f09942927b9752
Роман Меуш <@local.itstep>
2020-02-17 21:06:18 +0600

    Добавьте файлы проекта.

---
+++ MVC_Ajax/Controllers/MyController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Ajax.Models;
+
+namespace MVC_Ajax.Controllers
+{
+    public class MyController : Controller
+    {
+        private readonly List<Books> db;
+        // GET: My
+
+        public MyController()
+        {
+            db = new List<Books>
+            {
+                new Books { Name = "Энциклопедия", Author = "Marks" },
+                new Books { Name = "Война и мир", Author = "Толстой" },
+                new Books { Name = "Словарь", Author = "Oksford" },
+                new Books { Name = "Bell", Author = "Kunth" },
+                new Books { Name = "Test", Author = "Mars" }              
+            };
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult BookSearch(string name)
+        {
+
+            var allbooks = db.Where(z => z.Author.Contains(name)).ToList();
+            if (allbooks.Count <= 0)
+            {
+                return new HttpUnauthorizedResult();
+            }
+            return PartialView(allbooks);
+        }
+    }
+}
---
+++ MVC_Ajax/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("MVC_Ajax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVC_Ajax")]
+[assembly: AssemblyCopyright("© , 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("0ba01083-5311-469d-b099-dbd0c281a68d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
