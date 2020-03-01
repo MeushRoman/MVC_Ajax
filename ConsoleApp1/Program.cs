b90911e9eeadd3c56144cbcbd9929dc0611dfa39
Roman Dark <amaii.sakura@gmail.com>
2020-03-01 17:18:51 +0600

    df

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation qe = new QuadraticEquation();
+
+            var res = qe.res(1, 2, 1);
+        }
+
+    }
+
+    public class result
+    {
+        public double d { get; set; }
+        public double x1 { get; set; }
+        public double x2 { get; set; }
+    }
+
+    public class QuadraticEquation
+    {
+        public result res(double a, double b, double c)
+        {
+            result r = new result();
+            r.d = b * b - 4 * a * c;
+
+            if (r.d > 0)
+            {
+                double sqrtD = (double)System.Math.Sqrt(r.d);
+
+                r.x1 = (-b + sqrtD) / (2 * a);
+                r.x2 = (-b - sqrtD) / (2 * a);
+            }
+            else if (r.d == 0)
+            {
+                r.x1 = r.x2 = -(b / 2 * a);
+            }
+
+            return r;
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -4,25 +4,85 @@ using UnitTestProject1;
 
 namespace UnitTestProject1
 {
+    public class result
+    {
+        public double d { get; set; }
+        public double x1 { get; set; }
+        public double x2 { get; set; }
+    }
+
+    public class QuadraticEquation
+    {
+        public result res(double a, double b, double c)
+        {
+            result r = new result();
+            r.d = b * b - 4 * a * c;
+
+            if (r.d > 0)
+            {
+                double sqrtD = (float)System.Math.Sqrt(r.d);
+
+                r.x1 = (-b + sqrtD) / (2 * a);
+                r.x2 = (-b - sqrtD) / (2 * a);
+            }
+            else if (r.d == 0)
+            {
+                r.x1 = r.x2 = -(b / 2 * a);
+            }
+
+            return r;
+        }
+    }
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
-            //arrange 
-            int x = 2;
-            int y = 2;
+            QuadraticEquation qe = new QuadraticEquation();
 
-            //act
-            int z = x + y;
+            double a = 2, b= -11, c = 5;
 
-            //assert   -expected   -actual
 
-            Assert.IsTrue(z==4);
-            Assert.IsNotNull(z);
-            Assert.AreNotSame(x, y);
-            
+            var r = qe.res(a, b, c);
+
+            Assert.IsTrue(r.d > 0);
+
+            Assert.IsTrue((a * Math.Pow(r.x1, 2) + b *r.x1 + c) == 0);
+
+            Assert.IsTrue((a * Math.Pow(r.x2, 2) + b *r.x2 + c) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            QuadraticEquation qe = new QuadraticEquation();
+
+            double a = 1, b = 2, c = 1;
+
+
+            var r = qe.res(a, b, c);
+
+            Assert.IsTrue(r.d == 0);
+
+            Assert.IsTrue((a * Math.Pow(r.x1, 2) + b * r.x1 + c) == 0);
+
+            Assert.IsTrue((a * Math.Pow(r.x2, 2) + b * r.x2 + c) == 0);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            QuadraticEquation qe = new QuadraticEquation();
+
+            double a = 1, b = 1, c = 1;
+
+
+            var r = qe.res(a, b, c);
+
+            Assert.IsTrue(r.d < 0);
+
         }
     }
 }
